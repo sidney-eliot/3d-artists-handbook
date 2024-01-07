59f356f8c8657e46889f0dbdab5d5f045bc0464f
Sidney <sidney.eliot@proton.me>
2024-01-07 22:00:11 +0100

    Quartz sync: Jan 7, 2024, 10:00 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -47,7 +47,7 @@ const config: QuartzConfig = {
       Plugin.FrontMatter(),
       Plugin.TableOfContents({ maxDepth: 3 }),
       Plugin.CreatedModifiedDate({
-        priority: ["frontmatter", "filesystem"], // you can add 'git' here for last modified from Git but this makes the build slower
+        priority: ["git", "frontmatter", "filesystem"], // you can add 'git' here for last modified from Git but this makes the build slower
       }),
       Plugin.SyntaxHighlighting(),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
